27edba0a053c8a9cadac1519db5658df56112cd3
murilovantini <m.henrique.vantini18@gmail.com>
2024-11-21 15:25:37 -0300

    feat: adicionar appbar e configuração temas de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_be_talent/styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'BeTalent',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: MyStyles.primary),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: const TextTheme(
+            // H1
+            titleLarge: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+            // H2
+            titleMedium: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+            // H3
+            titleSmall: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+          )),
       home: const MyHomePage(),
     );
   }
@@ -24,30 +33,40 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+        backgroundColor: Colors.white,
+        toolbarHeight: 68,
+        leading: Transform.translate(
+          offset: const Offset(20, 0),
+          child: const CircleAvatar(
+            radius: 106.38,
+            backgroundColor: MyStyles.gray05,
+            child: Text('MV'),
+          ),
         ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications_rounded)),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 24),
+              Text('Funcionários', style: Theme.of(context).textTheme.titleLarge),
+            ],
+          ),
+        ),
       ),
     );
   }
