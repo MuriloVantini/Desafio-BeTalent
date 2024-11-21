d472336aa1abd87e3321da6c63ef049ba4e274aa
murilovantini <m.henrique.vantini18@gmail.com>
2024-11-21 15:25:20 -0300

    feat: criar classe para estilos estáticos

---
+++ lib/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyStyles {
+  static const Color primary = Color.fromRGBO(5, 0, 255, 1);
+  static const Color blue10 = Color.fromRGBO(237, 239, 251, 1);
+  static const Color black = Color.fromRGBO(28, 28, 28, 1);
+  static const Color gray20 = Color.fromRGBO(158, 158, 158, 1);
+  static const Color gray10 = Color.fromRGBO(223, 223, 223, 1);
+  static const Color gray05 = Color(0xFFF5F5F5);
+}
